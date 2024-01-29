ea4e3fcc1eb50afd8673e5307b67cb935444f47d
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2024-01-29 19:08:51 +0100

    Sett opp node server

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+console.log('\n========== ⚡ BOOTING UP ⚡ =========== \n')
+
+async function boot() {
+    try {
+
+    } catch(error) {
+        console.log('\n\n=========== 💥  TERROR 💥  ============\n\n')
+        console.log(error)
+    }
+}
+
+void boot()
