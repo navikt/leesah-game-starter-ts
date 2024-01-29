12ef5cdd427cc697045cc8d9b1c0e1d7b613e685
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2024-01-29 19:30:04 +0100

    Sette opp producer

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,21 @@
+import { Kafka, Partitioners } from "kafkajs"
+
 console.log('\n========== ⚡ BOOTING UP ⚡ =========== \n')
 
 async function boot() {
     try {
+        const host = process.env.HOST_IP
+
+        const kafka = new Kafka({
+            clientId: 'leesah-game-starter-js',
+            brokers: [`${host}:9092`]
+        })
+
+        const producer = kafka.producer({ createPartitioner: Partitioners.DefaultPartitioner })
+
+        await producer.connect()
+        
+        await producer.send({topic: 'topic-test', messages: [{value: 'Hello kafka!'}]})
 
     } catch(error) {
         console.log('\n\n=========== 💥  TERROR 💥  ============\n\n')
